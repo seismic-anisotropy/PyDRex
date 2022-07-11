6faa0f7d49144e04f565fb2213f65c73ee4d95b4
adigitoleo <adigitoleo@dissimulo.com>
2022-07-12 01:55:15 +1000

    test: Add 2D simple shear test
    Adds a 2D simple shear test. Optionally save the output figure with:
    
        pytest --outdir="out/"
    
    where "out/" is the name of an existing directory (can be "." for cwd).
    
    Have been tweaking the tolerances for a while, hopefully not too flaky...

---
+++ src/pydrex/visualisation.py
@@ -0,0 +1,50 @@
+"""PyDRex: Visualisation helpers for tests/examples."""
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def simple_shear_2d(
+    angles,
+    indices,
+    timestop,
+    savefile="pydrex_simlpe_shear_2d.png",
+    markers=("."),
+    labels=None,
+):
+    """Plot diagnostics for 2D simple shear tests."""
+    fig = plt.figure(figsize=(5, 8), dpi=300)
+    grid = fig.add_gridspec(2, 1, hspace=0.05)
+    ax_mean = fig.add_subplot(grid[0])
+    ax_mean.set_ylabel("Mean angle ∈ [0, 90]°")
+    ax_mean.axhline(0, color="k")
+    ax_mean.tick_params(labelbottom=False)
+    ax_strength = fig.add_subplot(grid[1], sharex=ax_mean)
+    ax_strength.set_ylabel("Texture strength (M-index)")
+    ax_strength.set_xlabel(r"Strain-time ($\dot{ε}_0 t$)")
+
+    if len(angles) % len(markers) != 0:
+        raise ValueError(
+            "Number of data series must be divisible by number of markers."
+            + f" You've supplied {len(angles)} Minerals and {len(markers)} markers."
+        )
+
+    for i, (misorient_angles, misorient_indices) in enumerate(zip(angles, indices)):
+        timesteps = np.linspace(0, timestop, len(misorient_angles))
+        label = None
+        if labels is not None:
+            label = labels[int(i / (len(angles) / len(labels)))]
+        markerseq = markers[int(i / (len(angles) / len(markers)))]
+
+        ax_mean.plot(
+            timesteps, misorient_angles, markerseq, markersize=5, alpha=0.33, label=label
+        )
+        ax_strength.plot(
+            timesteps, misorient_indices, markerseq, markersize=5, alpha=0.33, label=label
+        )
+
+    ax_mean.plot(timesteps, 45 * np.exp(timesteps * (np.cos(np.pi) - 1)), "r--")
+
+    if labels is not None:
+        ax_mean.legend()
+
+    fig.savefig(savefile, bbox_inches="tight")
---
+++ tests/test_simple_shear_2d.py
@@ -0,0 +1,168 @@
+"""PyDRex: 2D simple shear tests.
+
+NOTE: In scipy, rotations are represented as a matrix
+that transforms [1, 0, 0] to the new a-axis vector.
+We need the inverse of that rotation,
+which represents the change of coordinates
+from the grain-local to the global (Eulerian) frame.
+
+"""
+
+import numpy as np
+from numpy import random as rn
+from scipy.spatial.transform import Rotation
+
+from pydrex import minerals as _minerals
+from pydrex import deformation_mechanism as _defmech
+from pydrex import diagnostics as _diagnostics
+from pydrex import visualisation as _vis
+
+
+class TestSinglePolycrystalOlivineA:
+    """Tests for a single A-type olivine polycrystal in 2D simple shear."""
+
+    def test_shearYZ_initQ1(
+        self,
+        params_Kaminski2001_fig5_solid,
+        params_Kaminski2001_fig5_shortdash,
+        params_Kaminski2001_fig5_longdash,
+        outdir,
+    ):
+        """Test clockwise a-axis rotation around X.
+
+        Initial condition: randomised a-axis orientation within the first
+        quadrant (between +Y and +Z axes) and steady flow with velocity gradient
+
+                0 0 0
+            L = 0 0 2
+                0 0 0
+
+        Similar to Fig. 5 in [Kaminski 2001]
+
+        [Kaminski 2001]: https://doi.org/10.1016%2Fs0012-821x%2801%2900356-9
+
+        """
+        strain_rate_scale = 2e-5
+        velocity_gradient = np.zeros((3, 3))
+        velocity_gradient[1, 2] = strain_rate_scale
+        timescale = 1 / (strain_rate_scale / 2)
+        n_grains = 1000
+
+        # Initial orientations in first quadrant of YZ plane.
+        # This means that the starting average is the same,
+        # which is convenient for comparing the texture evolution.
+        orientations_init = (
+            Rotation.from_euler(
+                "zxz",
+                [
+                    [x * np.pi / 2, np.pi / 2, np.pi / 2]
+                    for x in rn.default_rng().random(n_grains)
+                ],
+            )
+            .inv()
+            .as_matrix()
+        )
+
+        # One mineral to test each value of grain boundary mobility.
+        minerals = [
+            _minerals.Mineral(
+                _minerals.MineralPhase.olivine,
+                _minerals.OlivineFabric.A,
+                _defmech.Regime.dislocation,
+                n_grains=n_grains,
+                fractions_init=np.full(n_grains, 1 / n_grains),
+                orientations_init=orientations_init,
+            )
+            for i in range(3)
+        ]
+
+        # Optional plotting setup.
+        if outdir is not None:
+            labels = []
+            angles = []
+            indices = []
+
+        for mineral, params in zip(
+            minerals,
+            (
+                params_Kaminski2001_fig5_solid,
+                params_Kaminski2001_fig5_shortdash,
+                params_Kaminski2001_fig5_longdash,
+            ),
+        ):
+            time = 0
+            timestep = 0.025
+            timestop = 1
+            deformation_gradient = np.eye(3)  # Undeformed initial state.
+            while time < timestop * timescale:
+                deformation_gradient = mineral.update_orientations(
+                    params,
+                    deformation_gradient,
+                    velocity_gradient,
+                    integration_time=timestep * timescale,
+                )
+                time += timestep * timescale
+
+            n_timesteps = len(mineral._orientations)
+            misorient_angles = np.zeros(n_timesteps)
+            misorient_indices = np.zeros(n_timesteps)
+            # Loop over first dimension (time steps) of orientations.
+            for idx, matrices in enumerate(mineral._orientations):
+                orientations_resampled, _ = _diagnostics.resample_orientations(
+                    matrices, mineral._fractions[idx]
+                )
+                direction_mean = _diagnostics.bingham_average(
+                    orientations_resampled,
+                    axis=_minerals.get_primary_axis(mineral.fabric),
+                )
+                misorient_angles[idx] = _diagnostics.smallest_angle(
+                    direction_mean, [0, 1, 0]
+                )
+                misorient_indices[idx] = _diagnostics.misorientation_index(
+                    orientations_resampled
+                )
+
+            # Check for uncorrupted record of initial condition.
+            assert np.isclose(misorient_angles[0], 45.0, atol=3.5)
+            assert misorient_indices[0] < 0.2
+            # Check for mostly smoothly decreasing misalignment.
+            angles_diff = np.diff(misorient_angles)
+            assert np.max(angles_diff) < 3.0
+            assert np.min(angles_diff) > -7.5
+            assert np.sum(angles_diff) < -25.0
+
+            # Check alignment and texture strength (half way and final value).
+            halfway = round(n_timesteps / 2)
+            match params["gbm_mobility"]:
+                case 0:
+                    assert np.isclose(misorient_angles[halfway], 25, atol=2.0)
+                    assert np.isclose(misorient_angles[-1], 17.0, atol=1.0)
+                    assert np.isclose(misorient_indices[halfway], 0.3, atol=0.075)
+                    assert np.isclose(misorient_indices[-1], 0.7, atol=0.05)
+                case 50:
+                    assert np.isclose(misorient_angles[halfway], 15, atol=1.5)
+                    assert np.isclose(misorient_angles[-1], 10, atol=1.0)
+                    assert np.isclose(misorient_indices[halfway], 0.65, atol=0.075)
+                    assert np.isclose(misorient_indices[-1], 0.85, atol=0.03)
+                case 200:
+                    assert np.isclose(misorient_angles[halfway], 9, atol=1.0)
+                    assert np.isclose(misorient_angles[-1], 7, atol=1.0)
+                    assert np.isclose(misorient_indices[halfway], 0.88, atol=0.05)
+                    assert np.isclose(misorient_indices[-1], 0.95, atol=0.03)
+
+            # Optionally store plotting metadata.
+            if outdir is not None:
+                labels.append(f"M* = {params['gbm_mobility']}")
+                angles.append(misorient_angles)
+                indices.append(misorient_indices)
+
+        # Optionally plot figure.
+        if outdir is not None:
+            _vis.simple_shear_2d(
+                angles,
+                indices,
+                timestop=timestop,
+                savefile=f"{outdir}/simple_shearYZ_single_olivineA_initQ1.png",
+                markers=("o", "v", "s"),
+                labels=labels,
+            )
