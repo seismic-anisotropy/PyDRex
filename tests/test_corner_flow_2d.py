be2b1c84b551016a1a7bb40b1b5949c6e3616d85
adigitoleo <adigitoleo@dissimulo.com>
2022-09-29 19:34:09 +1000

    test: Add prescribed pathline corner flow test

--- tests/test_corner_flow_2d.py
+++ tests/test_corner_flow_2d.py
@@ -220,6 +220,164 @@ class TestOlivineA:
                 zlims=(-domain_height, 0),
             )
 
+    def test_corner_pathline_prescribed_init_random(
+        self,
+        params_Kaminski2001_fig5_shortdash,
+        outdir,
+    ):
+        """Test CPO evolution during forward integration along a pathline.
+
+        Initial condition: fully random orientations in all 4 `Mineral`s.
+
+        Plate velocity: 2 cm/yr
+
+        """
+        # Plate velocity (half spreading rate), convert cm/yr to m/s.
+        plate_velocity = 2.0 / (100.0 * 365.0 * 86400.0)
+        domain_height = 1.0  # Normalised to olivine-spinel transition.
+        domain_width = 5.0
+        n_grains = 1000
+        orientations_init = Rotation.random(n_grains, random_state=1).as_matrix()
+        n_timesteps = 20  # Number of places along the pathline to compute CPO.
+
+        # Optional plotting setup.
+        if outdir is not None:
+            labels = []
+            angles = []
+            indices = []
+            r_paths = []
+            θ_paths = []
+            directions = []
+            timestamps = []
+
+        # Callables used to prescribe the macroscopic fields.
+        @nb.njit
+        def _get_velocity(point):
+            x, _, z = point[0]  # Expects a 2D array for the coords.
+            θ = np.arctan2(x, -z)
+            # Return with an extra dimension of shape 1, like scipy RBF.
+            return np.atleast_2d(get_velocity(θ, plate_velocity))
+
+        @nb.njit
+        def _get_velocity_gradient(point):
+            x, _, z = point[0]  # Expects a 2D array for the coords.
+            θ = np.arctan2(x, -z)
+            # Return with an extra dimension of shape 1, like scipy RBF.
+            velocity_gradient = get_velocity_gradient(θ, plate_velocity)
+            return np.reshape(velocity_gradient, (1, *velocity_gradient.shape))
+
+        # Note: θ values are in radians.
+        for z_exit in (-0.1, -0.3, -0.54, -0.78):
+            mineral = _minerals.Mineral(
+                _minerals.MineralPhase.olivine,
+                _minerals.OlivineFabric.A,
+                _defmech.Regime.dislocation,
+                n_grains=n_grains,
+                fractions_init=np.full(n_grains, 1 / n_grains),
+                orientations_init=orientations_init,
+            )
+            r_exit = np.sqrt(z_exit**2 + domain_width**2)
+            θ_exit = np.arccos(z_exit / r_exit)
+            timestamps_back, get_position = _pathlines.get_pathline(
+                np.array([domain_width, 0.0, z_exit]),
+                _get_velocity,
+                _get_velocity_gradient,
+                min_coords=np.array([0.0, 0.0, -domain_height]),
+                max_coords=np.array([domain_width, 0.0, 0.0]),
+            )
+            r_vals = []
+            θ_vals = []
+            deformation_gradient = np.eye(3)
+            times = np.linspace(timestamps_back[-1], timestamps_back[0], n_timesteps)
+            for time_start, time_end in it.pairwise(times):
+                deformation_gradient = mineral.update_orientations(
+                    params_Kaminski2001_fig5_shortdash,
+                    deformation_gradient,
+                    _get_velocity_gradient,
+                    integration_time=time_end - time_start,
+                    # FIXME: get_position seems to get stuck, need pathline tests
+                    pathline=(time_start, time_end, get_position),
+                )
+                x_current, _, z_current = get_position(time_end)
+                r_current = np.sqrt(x_current**2 + z_current**2)
+                θ_current = np.arctan2(x_current, -z_current)
+                if outdir is not None:
+                    r_vals.append(r_current)
+                    θ_vals.append(θ_current)
+
+            r_vals.append(r_exit)
+            θ_vals.append(θ_exit)
+
+            _log.info("final deformation gradient:\n%s", deformation_gradient)
+            assert (
+                n_timesteps
+                == len(mineral.orientations)
+                == len(mineral.fractions)
+                == len(r_vals)
+                == len(θ_vals)
+            ), (
+                f"n_timesteps = {n_timesteps}\n"
+                + f"len(mineral.orientations) = {len(mineral.orientations)}\n"
+                + f"len(mineral.fractions) = {len(mineral.fractions)}\n"
+                + f"len(r_vals) = {len(r_vals)}\n"
+                + f"len(θ_vals) = {len(θ_vals)}\n"
+            )
+
+            misorient_angles = np.zeros(n_timesteps)
+            misorient_indices = np.zeros(n_timesteps)
+            bingham_vectors = np.zeros((n_timesteps, 3))
+            # Loop over first dimension (time steps) of orientations.
+            for idx, matrices in enumerate(mineral.orientations):
+                orientations_resampled, _ = _diagnostics.resample_orientations(
+                    matrices, mineral.fractions[idx]
+                )
+                direction_mean = _diagnostics.bingham_average(
+                    orientations_resampled,
+                    axis=_minerals.get_primary_axis(mineral.fabric),
+                )
+                misorient_angles[idx] = _diagnostics.smallest_angle(
+                    direction_mean,
+                    [1, 0, 0],
+                )
+                misorient_indices[idx] = _diagnostics.misorientation_index(
+                    orientations_resampled
+                )
+                bingham_vectors[idx] = direction_mean
+
+            # TODO: Do the asserts here.
+            # angles_diff = np.diff(misorient_angles)
+            # assert np.max(angles_diff) < ...
+            # assert np.min(angles_diff) > ...
+            # assert np.sum(angles_diff) < ...
+
+            if outdir is not None:
+                mineral.save(
+                    f"{outdir}/corner_olivineA_pathline_prescribed_{str(z_exit).replace('.', 'd')}.npz"
+                )
+                # TODO: Also save processed data?
+                labels.append(rf"$z_{{f}}$ = {z_exit}")
+                angles.append(misorient_angles)
+                indices.append(misorient_indices)
+                r_paths.append(r_vals)
+                θ_paths.append(θ_vals)
+                timestamps.append(times)
+                directions.append(bingham_vectors)
+
+        if outdir is not None:
+            _vis.corner_flow_nointerp_2d(
+                angles,
+                indices,
+                r_paths,
+                θ_paths,
+                directions,
+                timestamps,
+                xlabel=f"x ⇀ ({plate_velocity:.2e} m/s)",
+                savefile=f"{outdir}/cornerXZ_olivineA_pathline_prescribed.png",
+                markers=("o", "v", "s", "p"),
+                labels=labels,
+                xlims=(0, domain_width),
+                zlims=(-domain_height, 0),
+            )
 
 #     @pytest.mark.wip
 #     def test_corner_pathline_prescribed_init_random(
