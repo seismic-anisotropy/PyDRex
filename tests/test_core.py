401ac1a052c55b3acd8147b4969376f726995d49
adigitoleo <adigitoleo@dissimulo.com>
2023-03-07 14:56:07 +1100

    docs: Fix documentation of core tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,34 +2,31 @@
 import numpy as np
 from scipy.spatial.transform import Rotation
 
-import pydrex.core as _core
-import pydrex.minerals as _minerals
+from pydrex import core as _core
+from pydrex import minerals as _minerals
+from pydrex import logger as _log
 
 
-class TestSimpleShearSingleGrains:
-    """Single-grain analytical re-orientation rate tests."""
-
-    # Kaminski used passive rotations (see Kaminski 2001 eq. 1) and ZXZ convention.
-    # Scipy gives us active rotations by default,
-    # and for composite rotations the `from_rotvec` method uses the XYZ convention.
-    # Therefore, for composite rotations we should use `from_euler("ZXZ", ...)`.
-    # This will give us active rotations in the ZXZ convention, i.e.
-    #
-    #            c1c3 - c2s1s3 |  -c1s3 - c2c3s1 |   s1s2
-    # Z1 X1 Z2 = c3s1 + c2c3s1 |   c1c2c3 - s1s3 |  -c1s2
-    #            s2s3          |   c3s2          |   c2
-    #
-    # where ci = cos(angle of i'th elementary rotation), si = sin(...),
-    # which is the same as the passive version but transposed.
+class TestSimpleShearSingleAType:
+    """Single-grain A-type olivine analytical re-orientation rate tests."""
 
     def test_initP10Z(self):
-        # Single grain of olivine A-type, simple shear with:
-        #     0 0 0  .   0 1 0      0 -1 0
-        # L = 2 0 0  ε = 1 0 0  Ω = 1  0 0
-        #     0 0 0      0 0 0      0  0 0
+        r"""Single grain of olivine A-type, simple shear with:
+
+        $$
+        \bm{L} = \begin{bmatrix} 0 & 0 & 0 \cr 2 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\dot{\varepsilon}} = \begin{bmatrix} 0 & 1 & 0 \cr 1 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\Omega} = \begin{bmatrix} 0 & -1 & 0 \cr 1 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix}
+        $$
+
+        Initial rotation 10° anti-clockwise around Z (looking down the axis).
+
+        """
         nondim_velocity_gradient = np.array([[0, 0, 0], [2.0, 0, 0], [0, 0, 0]])
         nondim_strain_rate = np.array([[0, 1.0, 0], [1.0, 0, 0], [0, 0, 0]])
-        # Grain initialised with rotation around Z (active rotation convention).
+        # Grain initialised with rotation around Z (anti-clockwise).
         θ = np.deg2rad(10)
         initial_orientations = Rotation.from_rotvec([[0, 0, θ]])
         orientations_diff, fractions_diff = _core.derivatives(
@@ -49,7 +46,7 @@ class TestSimpleShearSingleGrains:
         cosθ = np.cos(θ)
         cos2θ = np.cos(2 * θ)
         sinθ = np.sin(θ)
-        print("calculated re-orientation rate:\n", orientations_diff)
+        _log.debug("calculated re-orientation rate:\n", orientations_diff)
         target_orientations_diff = np.array(
             [
                 [sinθ * (1 + cos2θ), cosθ * (1 + cos2θ), 0],
@@ -57,19 +54,28 @@ class TestSimpleShearSingleGrains:
                 [0, 0, 0],
             ]
         )
-        print("target re-orientation rate:\n", target_orientations_diff)
+        _log.debug("target re-orientation rate:\n", target_orientations_diff)
         assert np.allclose(orientations_diff, target_orientations_diff)
-        print("calculated grain volume change:\n", fractions_diff)
+        _log.debug("calculated grain volume change:\n", fractions_diff)
         assert np.isclose(np.sum(fractions_diff), 0.0)
 
     def test_initN10Z(self):
-        # Single grain of olivine A-type, simple shear with:
-        #     0 0 0  .   0 1 0      0 -1 0
-        # L = 2 0 0  ε = 1 0 0  Ω = 1  0 0
-        #     0 0 0      0 0 0      0  0 0
+        r"""Single grain of olivine A-type, simple shear with:
+
+        $$
+        \bm{L} = \begin{bmatrix} 0 & 0 & 0 \cr 2 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\dot{\varepsilon}} = \begin{bmatrix} 0 & 1 & 0 \cr 1 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\Omega} = \begin{bmatrix} 0 & -1 & 0 \cr 1 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix}
+        $$
+
+        Initial rotation 10° clockwise around Z (looking down the axis).
+
+        """
         nondim_velocity_gradient = np.array([[0, 0, 0], [2.0, 0, 0], [0, 0, 0]])
         nondim_strain_rate = np.array([[0, 1.0, 0], [1.0, 0, 0], [0, 0, 0]])
-        # Grain initialised with rotation around Z (active rotation convention).
+        # Grain initialised with rotation around Z (clockwise).
         θ = np.deg2rad(-10)
         initial_orientations = Rotation.from_rotvec([[0, 0, θ]])
         orientations_diff, fractions_diff = _core.derivatives(
@@ -89,7 +95,7 @@ class TestSimpleShearSingleGrains:
         cosθ = np.cos(θ)
         cos2θ = np.cos(2 * θ)
         sinθ = np.sin(θ)
-        print("calculated re-orientation rate:\n", orientations_diff)
+        _log.debug("calculated re-orientation rate:\n", orientations_diff)
         target_orientations_diff = np.array(
             [
                 [sinθ * (1 + cos2θ), cosθ * (1 + cos2θ), 0],
@@ -97,19 +103,28 @@ class TestSimpleShearSingleGrains:
                 [0, 0, 0],
             ]
         )
-        print("target re-orientation rate:\n", target_orientations_diff)
+        _log.debug("target re-orientation rate:\n", target_orientations_diff)
         assert np.allclose(orientations_diff, target_orientations_diff)
-        print("calculated grain volume changes:\n", fractions_diff)
+        _log.debug("calculated grain volume changes:\n", fractions_diff)
         assert np.isclose(np.sum(fractions_diff), 0.0)
 
     def test_initP45Z(self):
-        # Single grain of olivine A-type, simple shear with:
-        #     0 0 0  .   0 1 0      0 -1 0
-        # L = 2 0 0  ε = 1 0 0  Ω = 1  0 0
-        #     0 0 0      0 0 0      0  0 0
+        r"""Single grain of olivine A-type, simple shear with:
+
+        $$
+        \bm{L} = \begin{bmatrix} 0 & 0 & 0 \cr 2 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\dot{\varepsilon}} = \begin{bmatrix} 0 & 1 & 0 \cr 1 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\Omega} = \begin{bmatrix} 0 & -1 & 0 \cr 1 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix}
+        $$
+
+        Initial rotation 45° anti-clockwise around Z (looking down the axis).
+
+        """
         nondim_velocity_gradient = np.array([[0, 0, 0], [2.0, 0, 0], [0, 0, 0]])
         nondim_strain_rate = np.array([[0, 1.0, 0], [1.0, 0, 0], [0, 0, 0]])
-        # Grain initialised with rotation around Z (active rotation convention).
+        # Grain initialised with rotation around Z (anti-clockwise).
         θ = np.deg2rad(45)
         initial_orientations = Rotation.from_rotvec([[0, 0, θ]])
         orientations_diff, fractions_diff = _core.derivatives(
@@ -129,7 +144,7 @@ class TestSimpleShearSingleGrains:
         cosθ = np.cos(θ)
         cos2θ = np.cos(2 * θ)
         sinθ = np.sin(θ)
-        print("calculated re-orientation rate:\n", orientations_diff)
+        _log.debug("calculated re-orientation rate:\n", orientations_diff)
         target_orientations_diff = np.array(
             [
                 [sinθ * (1 + cos2θ), cosθ * (1 + cos2θ), 0],
@@ -137,19 +152,28 @@ class TestSimpleShearSingleGrains:
                 [0, 0, 0],
             ]
         )
-        print("target re-orientation rate:\n", target_orientations_diff)
+        _log.debug("target re-orientation rate:\n", target_orientations_diff)
         assert np.allclose(orientations_diff, target_orientations_diff)
-        print("calculated grain volume changes:\n", fractions_diff)
+        _log.debug("calculated grain volume changes:\n", fractions_diff)
         assert np.isclose(np.sum(fractions_diff), 0.0)
 
     def test_initP90Z(self):
-        # Single grain of olivine A-type, simple shear with:
-        #     0 0 0  .   0 1 0      0 -1 0
-        # L = 2 0 0  ε = 1 0 0  Ω = 1  0 0
-        #     0 0 0      0 0 0      0  0 0
+        r"""Single grain of olivine A-type, simple shear with:
+
+        $$
+        \bm{L} = \begin{bmatrix} 0 & 0 & 0 \cr 2 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\dot{\varepsilon}} = \begin{bmatrix} 0 & 1 & 0 \cr 1 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\Omega} = \begin{bmatrix} 0 & -1 & 0 \cr 1 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix}
+        $$
+
+        Initial rotation 90° anti-clockwise around Z (looking down the axis).
+
+        """
         nondim_velocity_gradient = np.array([[0, 0, 0], [2.0, 0, 0], [0, 0, 0]])
         nondim_strain_rate = np.array([[0, 1.0, 0], [1.0, 0, 0], [0, 0, 0]])
-        # Grain initialised with rotation around Z (active rotation convention).
+        # Grain initialised with rotation around Z (anti-clockwise).
         θ = np.deg2rad(90)
         initial_orientations = Rotation.from_rotvec([[0, 0, θ]])
         orientations_diff, fractions_diff = _core.derivatives(
@@ -169,7 +193,7 @@ class TestSimpleShearSingleGrains:
         cosθ = np.cos(θ)
         cos2θ = np.cos(2 * θ)
         sinθ = np.sin(θ)
-        print("calculated re-orientation rate:\n", orientations_diff)
+        _log.debug("calculated re-orientation rate:\n", orientations_diff)
         target_orientations_diff = np.array(
             [
                 [sinθ * (1 + cos2θ), cosθ * (1 + cos2θ), 0],
@@ -177,19 +201,28 @@ class TestSimpleShearSingleGrains:
                 [0, 0, 0],
             ]
         )
-        print("target re-orientation rate:\n", target_orientations_diff)
+        _log.debug("target re-orientation rate:\n", target_orientations_diff)
         assert np.allclose(orientations_diff, target_orientations_diff)
-        print("calculated grain volume changes:\n", fractions_diff)
+        _log.debug("calculated grain volume changes:\n", fractions_diff)
         assert np.isclose(np.sum(fractions_diff), 0.0)
 
     def test_initP10Y(self):
-        # Single grain of olivine A-type, simple shear with:
-        #     0 0 2  .   0 0 1       0 0 1
-        # L = 0 0 0  ε = 0 0 0  Ω =  0 0 0
-        #     0 0 0      1 0 0      -1 0 0
+        r"""Single grain of olivine A-type, simple shear with:
+
+        $$
+        \bm{L} = \begin{bmatrix} 0 & 0 & 2 \cr 0 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\dot{\varepsilon}} = \begin{bmatrix} 0 & 0 & 1 \cr 0 & 0 & 0 \cr 1 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\Omega} = \begin{bmatrix} 0 & 0 & 1 \cr 0 & 0 & 0 \cr -1 & 0 & 0 \end{bmatrix}
+        $$
+
+        Initial rotation 10° anti-clockwise around Y (looking down the axis).
+
+        """
         nondim_velocity_gradient = np.array([[0, 0, 2.0], [0, 0, 0], [0, 0, 0]])
         nondim_strain_rate = np.array([[0, 0, 1.0], [0, 0, 0], [1.0, 0, 0]])
-        # Grain initialised with rotation around Y (active rotation convention).
+        # Grain initialised with rotation around Y (anti-clockwise).
         θ = np.deg2rad(10)
         initial_orientations = Rotation.from_rotvec([[0, θ, 0]])
         orientations_diff, fractions_diff = _core.derivatives(
@@ -209,7 +242,7 @@ class TestSimpleShearSingleGrains:
         cosθ = np.cos(θ)
         cos2θ = np.cos(2 * θ)
         sinθ = np.sin(θ)
-        print("calculated re-orientation rate:\n", orientations_diff)
+        _log.debug("calculated re-orientation rate:\n", orientations_diff)
         target_orientations_diff = np.array(
             [
                 [sinθ * (1 - cos2θ), 0, cosθ * (cos2θ - 1)],
@@ -217,19 +250,28 @@ class TestSimpleShearSingleGrains:
                 [cosθ * (1 - cos2θ), 0, sinθ * (1 - cos2θ)],
             ]
         )
-        print("target re-orientation rate:\n", target_orientations_diff)
+        _log.debug("target re-orientation rate:\n", target_orientations_diff)
         assert np.allclose(orientations_diff, target_orientations_diff)
-        print("calculated grain volume changes:\n", fractions_diff)
+        _log.debug("calculated grain volume changes:\n", fractions_diff)
         assert np.isclose(np.sum(fractions_diff), 0.0)
 
     def test_initN10Y(self):
-        # Single grain of olivine A-type, simple shear with:
-        #     0 0 2  .   0 0 1       0 0 1
-        # L = 0 0 0  ε = 0 0 0  Ω =  0 0 0
-        #     0 0 0      1 0 0      -1 0 0
+        r"""Single grain of olivine A-type, simple shear with:
+
+        $$
+        \bm{L} = \begin{bmatrix} 0 & 0 & 2 \cr 0 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\dot{\varepsilon}} = \begin{bmatrix} 0 & 0 & 1 \cr 0 & 0 & 0 \cr 1 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\Omega} = \begin{bmatrix} 0 & 0 & 1 \cr 0 & 0 & 0 \cr -1 & 0 & 0 \end{bmatrix}
+        $$
+
+        Initial rotation 10° clockwise around Y (looking down the axis).
+
+        """
         nondim_velocity_gradient = np.array([[0, 0, 2.0], [0, 0, 0], [0, 0, 0]])
         nondim_strain_rate = np.array([[0, 0, 1.0], [0, 0, 0], [1.0, 0, 0]])
-        # Grain initialised with rotation around Y (active rotation convention).
+        # Grain initialised with rotation around Y (clockwise).
         θ = np.deg2rad(-10)
         initial_orientations = Rotation.from_rotvec([[0, θ, 0]])
         orientations_diff, fractions_diff = _core.derivatives(
@@ -249,7 +291,7 @@ class TestSimpleShearSingleGrains:
         cosθ = np.cos(θ)
         cos2θ = np.cos(2 * θ)
         sinθ = np.sin(θ)
-        print("calculated re-orientation rate:\n", orientations_diff)
+        _log.debug("calculated re-orientation rate:\n", orientations_diff)
         target_orientations_diff = np.array(
             [
                 [sinθ * (1 - cos2θ), 0, cosθ * (cos2θ - 1)],
@@ -257,19 +299,28 @@ class TestSimpleShearSingleGrains:
                 [cosθ * (1 - cos2θ), 0, sinθ * (1 - cos2θ)],
             ]
         )
-        print("target re-orientation rate:\n", target_orientations_diff)
+        _log.debug("target re-orientation rate:\n", target_orientations_diff)
         assert np.allclose(orientations_diff, target_orientations_diff)
-        print("calculated grain volume changes:\n", fractions_diff)
+        _log.debug("calculated grain volume changes:\n", fractions_diff)
         assert np.isclose(np.sum(fractions_diff), 0.0)
 
     def test_initP45Y(self):
-        # Single grain of olivine A-type, simple shear with:
-        #     0 0 2  .   0 0 1       0 0 1
-        # L = 0 0 0  ε = 0 0 0  Ω =  0 0 0
-        #     0 0 0      1 0 0      -1 0 0
+        r"""Single grain of olivine A-type, simple shear with:
+
+        $$
+        \bm{L} = \begin{bmatrix} 0 & 0 & 2 \cr 0 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\dot{\varepsilon}} = \begin{bmatrix} 0 & 0 & 1 \cr 0 & 0 & 0 \cr 1 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\Omega} = \begin{bmatrix} 0 & 0 & 1 \cr 0 & 0 & 0 \cr -1 & 0 & 0 \end{bmatrix}
+        $$
+
+        Initial rotation 45° anti-clockwise around Y (looking down the axis).
+
+        """
         nondim_velocity_gradient = np.array([[0, 0, 2.0], [0, 0, 0], [0, 0, 0]])
         nondim_strain_rate = np.array([[0, 0, 1.0], [0, 0, 0], [1.0, 0, 0]])
-        # Grain initialised with rotation around Y (active rotation convention).
+        # Grain initialised with rotation around Y (anti-clockwise).
         θ = np.deg2rad(45)
         initial_orientations = Rotation.from_rotvec([[0, θ, 0]])
         orientations_diff, fractions_diff = _core.derivatives(
@@ -289,7 +340,7 @@ class TestSimpleShearSingleGrains:
         cosθ = np.cos(θ)
         cos2θ = np.cos(2 * θ)
         sinθ = np.sin(θ)
-        print("calculated re-orientation rate:\n", orientations_diff)
+        _log.debug("calculated re-orientation rate:\n", orientations_diff)
         target_orientations_diff = np.array(
             [
                 [sinθ * (1 - cos2θ), 0, cosθ * (cos2θ - 1)],
@@ -297,19 +348,28 @@ class TestSimpleShearSingleGrains:
                 [cosθ * (1 - cos2θ), 0, sinθ * (1 - cos2θ)],
             ]
         )
-        print("target re-orientation rate:\n", target_orientations_diff)
+        _log.debug("target re-orientation rate:\n", target_orientations_diff)
         assert np.allclose(orientations_diff, target_orientations_diff)
-        print("calculated grain volume changes:\n", fractions_diff)
+        _log.debug("calculated grain volume changes:\n", fractions_diff)
         assert np.isclose(np.sum(fractions_diff), 0.0)
 
     def test_initP90Y(self):
-        # Single grain of olivine A-type, simple shear with:
-        #     0 0 2  .   0 0 1       0 0 1
-        # L = 0 0 0  ε = 0 0 0  Ω =  0 0 0
-        #     0 0 0      1 0 0      -1 0 0
+        r"""Single grain of olivine A-type, simple shear with:
+
+        $$
+        \bm{L} = \begin{bmatrix} 0 & 0 & 2 \cr 0 & 0 & 0 \cr 0 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\dot{\varepsilon}} = \begin{bmatrix} 0 & 0 & 1 \cr 0 & 0 & 0 \cr 1 & 0 & 0 \end{bmatrix},
+        \quad
+        \bm{\Omega} = \begin{bmatrix} 0 & 0 & 1 \cr 0 & 0 & 0 \cr -1 & 0 & 0 \end{bmatrix}
+        $$
+
+        Initial rotation 90° anti-clockwise around Y (looking down the axis).
+
+        """
         nondim_velocity_gradient = np.array([[0, 0, 2.0], [0, 0, 0], [0, 0, 0]])
         nondim_strain_rate = np.array([[0, 0, 1.0], [0, 0, 0], [1.0, 0, 0]])
-        # Grain initialised with rotation around Y (active rotation convention).
+        # Grain initialised with rotation around Y (anti-clockwise).
         θ = np.deg2rad(90)
         initial_orientations = Rotation.from_rotvec([[0, θ, 0]])
         orientations_diff, fractions_diff = _core.derivatives(
@@ -329,7 +389,7 @@ class TestSimpleShearSingleGrains:
         cosθ = np.cos(θ)
         cos2θ = np.cos(2 * θ)
         sinθ = np.sin(θ)
-        print("calculated re-orientation rate:\n", orientations_diff)
+        _log.debug("calculated re-orientation rate:\n", orientations_diff)
         target_orientations_diff = np.array(
             [
                 [sinθ * (1 - cos2θ), 0, cosθ * (cos2θ - 1)],
@@ -337,7 +397,7 @@ class TestSimpleShearSingleGrains:
                 [cosθ * (1 - cos2θ), 0, sinθ * (1 - cos2θ)],
             ]
         )
-        print("target re-orientation rate:\n", target_orientations_diff)
+        _log.debug("target re-orientation rate:\n", target_orientations_diff)
         assert np.allclose(orientations_diff, target_orientations_diff)
-        print("calculated grain volume changes:\n", fractions_diff)
+        _log.debug("calculated grain volume changes:\n", fractions_diff)
         assert np.isclose(np.sum(fractions_diff), 0.0)
