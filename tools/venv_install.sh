2067764553bfb203a702efa5234028eebe98f346
adigitoleo <adigitoleo@posteo.net>
2023-10-18 11:37:03 +1100

    dev: Improve venv install script

--- tools/venv_install.sh
+++ tools/venv_install.sh
@@ -1,4 +1,4 @@
-#!/bin/env sh
+#!/bin/env bash
 set -eu
 readonly SCRIPTNAME="${0##*/}"
 usage() {
@@ -21,6 +21,7 @@ helpf() {
     echo
     echo 'Backups of the last requirements.txt are stored in requirements.bak'
 }
+warn() { >&2 printf '%s\n' "$SCRIPTNAME: $1"; }
 
 upgrade() {
     . .venv-"${PWD##*/}"/bin/activate
@@ -31,7 +32,14 @@ upgrade() {
 
 if [ $# -eq 0 ]; then  # first install
     [ -d .venv-"${PWD##*/}" ] && exit 0  # venv already exists, no-op
-    "$(2>/dev/null pyenv prefix||printf '/usr')"/bin/python -m venv .venv-"${PWD##*/}"
+    if [ -z "${PYTHON_BINARY:-}" ]; then
+        PYTHON_BINARY="$(2>/dev/null pyenv prefix||printf '/usr')"/bin/python
+    fi
+    [ $($PYTHON_BINARY --version|cut -d' ' -f2|cut -d'.' -f1) -eq 3 ] || {
+        warn "Python 3 is required"; exit 1; }
+    [ $($PYTHON_BINARY --version|cut -d' ' -f2|cut -d'.' -f2) -gt 10 ] || {
+        warn "Python 3.11+ is required"; exit 1; }
+    $PYTHON_BINARY -m venv .venv-"${PWD##*/}"
     upgrade
 fi
 
