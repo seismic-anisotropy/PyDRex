fix: Change signature of velocity (gradient) callables to f(t, x)
Closes #180.

Note that we are now officially `f(t, x)` people. Any use of `f(x, t)`
will be met with scorn and disgust.

src/pydrex/pathlines.py
@@ -63,7 +63,7 @@ def get_pathline(
 
         if _is_inside(point, min_coords, max_coords):
             dε = _utils.strain_increment(
-                time - _time_prev, get_velocity_gradient(point)
+                time - _time_prev, get_velocity_gradient(np.nan, point)
             )
             if time > _time_prev:  # Timestamps jump around for SciPy to find the root.
                 _strain += dε
@@ -120,14 +120,14 @@ def get_pathline(
 def _ivp_func(time, point, get_velocity, get_velocity_gradient, min_coords, max_coords):
     """Internal use only, must have the same signature as `get_pathline`."""
     if _is_inside(point, min_coords, max_coords):
-        return get_velocity(point)
+        return get_velocity(np.nan, point)
     return np.zeros_like(point)
 
 
 def _ivp_jac(time, point, get_velocity, get_velocity_gradient, min_coords, max_coords):
     """Internal use only, must have the same signature as `_ivp_func`."""
     if _is_inside(point, min_coords, max_coords):
-        return get_velocity_gradient(point)
+        return get_velocity_gradient(np.nan, point)
     return np.zeros((np.array(point).size,) * 2)
 
 

tests/test_vortex_2d.py
@@ -101,7 +101,9 @@ class TestCellOlivineA:
             regular_steps=int(max_strain * 10),
         )
         positions = [get_position(t) for t in timestamps]
-        velocity_gradients = [get_velocity_gradient(np.asarray(x)) for x in positions]
+        velocity_gradients = [
+            get_velocity_gradient(np.nan, np.asarray(x)) for x in positions
+        ]
         strains = np.empty_like(timestamps)
         strains[0] = 0
         for t, time in enumerate(timestamps[:-1], start=1):
@@ -145,7 +147,7 @@ class TestCellOlivineA:
         )
         angles = [
             _diagnostics.smallest_angle(
-                _diagnostics.bingham_average(a, axis="a"), get_velocity(x)
+                _diagnostics.bingham_average(a, axis="a"), get_velocity(np.nan, x)
             )
             for a, x in zip(mineral.orientations, positions, strict=True)
         ]
@@ -233,7 +235,8 @@ class TestCellOlivineA:
                 timestamps, positions, strains, mineral, deformation_gradient = out
                 angles[s] = [
                     _diagnostics.smallest_angle(
-                        _diagnostics.bingham_average(a, axis="a"), get_velocity(x)
+                        _diagnostics.bingham_average(a, axis="a"),
+                        get_velocity(np.nan, x),
                     )
                     for a, x in zip(mineral.orientations, positions, strict=True)
                 ]